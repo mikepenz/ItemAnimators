f4dfb7c2886aa6a2603827482b41a5b7640ee072
Mike Penz <mikepenz@gmail.com>
2016-01-11 23:24:39 +0100

    * 4 new animators  * SlideInOutBottomAnimator  * SlideInOutTopAnimator  * SlideInOutRightAnimator  * SlideInOutLeftAnimator

---
+++ library/src/main/java/com/mikepenz/itemanimators/SlideInOutBottomAnimator.java
@@ -0,0 +1,69 @@
+package com.mikepenz.itemanimators;
+
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by mikepenz on 08.01.16.
+ */
+public class SlideInOutBottomAnimator extends DefaultAnimator<SlideInOutBottomAnimator> {
+
+    private float mDeltaY;
+    private RecyclerView mRecyclerView;
+
+    public SlideInOutBottomAnimator(RecyclerView recyclerView) {
+        mRecyclerView = recyclerView;
+    }
+
+    @Override
+    public void addAnimationPrepare(RecyclerView.ViewHolder holder) {
+        retrieveItemPosition(holder);
+        ViewCompat.setTranslationY(holder.itemView, -mDeltaY);
+        ViewCompat.setTranslationZ(holder.itemView, 100);
+    }
+
+    @Override
+    public ViewPropertyAnimatorCompat addAnimation(RecyclerView.ViewHolder holder) {
+        final View view = holder.itemView;
+        return ViewCompat.animate(view).translationY(0).alpha(1).setDuration(getAddDuration());
+    }
+
+    @Override
+    public void addAnimationCleanup(RecyclerView.ViewHolder holder) {
+        ViewCompat.setAlpha(holder.itemView, 1);
+        ViewCompat.setTranslationY(holder.itemView, 0);
+        ViewCompat.setTranslationZ(holder.itemView, 1);
+    }
+
+    @Override
+    public ViewPropertyAnimatorCompat removeAnimation(RecyclerView.ViewHolder holder) {
+        ViewCompat.setTranslationZ(holder.itemView, 100);
+        final ViewPropertyAnimatorCompat animation = ViewCompat.animate(holder.itemView);
+        return animation.setDuration(getRemoveDuration()).alpha(0).translationY(-mDeltaY);
+    }
+
+    @Override
+    public void removeAnimationCleanup(RecyclerView.ViewHolder holder) {
+        ViewCompat.setTranslationY(holder.itemView, 0);
+        ViewCompat.setAlpha(holder.itemView, 1);
+        ViewCompat.setTranslationZ(holder.itemView, 1);
+    }
+
+
+    private void retrieveItemPosition(final RecyclerView.ViewHolder holder) {
+        mDeltaY = mRecyclerView.getHeight() - mRecyclerView.getLayoutManager().getDecoratedTop(holder.itemView);
+    }
+
+    @Override
+    public long getAddDelay(long remove, long move, long change) {
+        return 0;
+    }
+
+    @Override
+    public long getRemoveDelay(long remove, long move, long change) {
+        return remove / 2;
+    }
+
+}
---
+++ library/src/main/java/com/mikepenz/itemanimators/SlideInOutLeftAnimator.java
@@ -0,0 +1,69 @@
+package com.mikepenz.itemanimators;
+
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by mikepenz on 08.01.16.
+ */
+public class SlideInOutLeftAnimator extends DefaultAnimator<SlideInOutLeftAnimator> {
+
+    private float mDeltaX;
+    private RecyclerView mRecyclerView;
+
+    public SlideInOutLeftAnimator(RecyclerView recyclerView) {
+        mRecyclerView = recyclerView;
+    }
+
+    @Override
+    public void addAnimationPrepare(RecyclerView.ViewHolder holder) {
+        retrieveItemPosition(holder);
+        ViewCompat.setTranslationX(holder.itemView, -mDeltaX);
+        ViewCompat.setTranslationZ(holder.itemView, 100);
+    }
+
+    @Override
+    public ViewPropertyAnimatorCompat addAnimation(RecyclerView.ViewHolder holder) {
+        final View view = holder.itemView;
+        return ViewCompat.animate(view).translationX(0).alpha(1).setDuration(getAddDuration());
+    }
+
+    @Override
+    public void addAnimationCleanup(RecyclerView.ViewHolder holder) {
+        ViewCompat.setAlpha(holder.itemView, 1);
+        ViewCompat.setTranslationX(holder.itemView, 0);
+        ViewCompat.setTranslationZ(holder.itemView, 1);
+    }
+
+    @Override
+    public ViewPropertyAnimatorCompat removeAnimation(RecyclerView.ViewHolder holder) {
+        ViewCompat.setTranslationZ(holder.itemView, 100);
+        final ViewPropertyAnimatorCompat animation = ViewCompat.animate(holder.itemView);
+        return animation.setDuration(getRemoveDuration()).alpha(0).translationX(-mDeltaX);
+    }
+
+    @Override
+    public void removeAnimationCleanup(RecyclerView.ViewHolder holder) {
+        ViewCompat.setTranslationX(holder.itemView, 0);
+        ViewCompat.setAlpha(holder.itemView, 1);
+        ViewCompat.setTranslationZ(holder.itemView, 1);
+    }
+
+
+    private void retrieveItemPosition(final RecyclerView.ViewHolder holder) {
+        mDeltaX = mRecyclerView.getWidth() - mRecyclerView.getLayoutManager().getDecoratedLeft(holder.itemView);
+    }
+
+    @Override
+    public long getAddDelay(long remove, long move, long change) {
+        return 0;
+    }
+
+    @Override
+    public long getRemoveDelay(long remove, long move, long change) {
+        return remove / 2;
+    }
+
+}
