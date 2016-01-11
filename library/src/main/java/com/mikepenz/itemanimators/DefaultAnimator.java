4537d341a5dec4adf728e1c1e32576f64ba7f49d
Mike Penz <mikepenz@gmail.com>
2016-01-11 19:43:57 +0100

    * change DefaultAnimator and BaseItemAnimator methods to pass ViewHolders instead only views

--- library/src/main/java/com/mikepenz/itemanimators/DefaultAnimator.java
+++ library/src/main/java/com/mikepenz/itemanimators/DefaultAnimator.java
@@ -1,8 +1,9 @@
 package com.mikepenz.itemanimators;
 
+
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPropertyAnimatorCompat;
-import android.view.View;
+import android.support.v7.widget.RecyclerView;
 
 /**
  * Created by mikepenz on 08.01.16.
@@ -10,38 +11,38 @@ import android.view.View;
 public class DefaultAnimator<T> extends BaseItemAnimator<T> {
     // ADD ANIMATION METHODS
 
-    public void addAnimationPrepare(View view) {
-        ViewCompat.setAlpha(view, 0);
+    public void addAnimationPrepare(RecyclerView.ViewHolder holder) {
+        ViewCompat.setAlpha(holder.itemView, 0);
     }
 
-    public ViewPropertyAnimatorCompat addAnimation(View view) {
-        return ViewCompat.animate(view).alpha(1).setDuration(getAddDuration()).setInterpolator(getInterpolator());
+    public ViewPropertyAnimatorCompat addAnimation(RecyclerView.ViewHolder holder) {
+        return ViewCompat.animate(holder.itemView).alpha(1).setDuration(getAddDuration()).setInterpolator(getInterpolator());
     }
 
-    public void addAnimationCleanup(View view) {
-        ViewCompat.setAlpha(view, 1);
+    public void addAnimationCleanup(RecyclerView.ViewHolder holder) {
+        ViewCompat.setAlpha(holder.itemView, 1);
     }
 
     // REMOVE ANIMATION METHODS
 
-    public ViewPropertyAnimatorCompat removeAnimation(View view) {
-        return ViewCompat.animate(view).setDuration(getRemoveDuration()).alpha(0).setInterpolator(getInterpolator());
+    public ViewPropertyAnimatorCompat removeAnimation(RecyclerView.ViewHolder holder) {
+        return ViewCompat.animate(holder.itemView).setDuration(getRemoveDuration()).alpha(0).setInterpolator(getInterpolator());
     }
 
-    public void removeAnimationCleanup(View view) {
-        ViewCompat.setAlpha(view, 1);
+    public void removeAnimationCleanup(RecyclerView.ViewHolder holder) {
+        ViewCompat.setAlpha(holder.itemView, 1);
     }
 
     // CHANGE ANIMATION METHODS
-    public ViewPropertyAnimatorCompat changeOldAnimation(View view, ChangeInfo changeInfo) {
-        return ViewCompat.animate(view).setDuration(getChangeDuration()).alpha(0).translationX(changeInfo.toX - changeInfo.fromX).translationY(changeInfo.toY - changeInfo.fromY).setInterpolator(getInterpolator());
+    public ViewPropertyAnimatorCompat changeOldAnimation(RecyclerView.ViewHolder holder, ChangeInfo changeInfo) {
+        return ViewCompat.animate(holder.itemView).setDuration(getChangeDuration()).alpha(0).translationX(changeInfo.toX - changeInfo.fromX).translationY(changeInfo.toY - changeInfo.fromY).setInterpolator(getInterpolator());
     }
 
-    public ViewPropertyAnimatorCompat changeNewAnimation(View view) {
-        return ViewCompat.animate(view).translationX(0).translationY(0).setDuration(getChangeDuration()).alpha(1).setInterpolator(getInterpolator());
+    public ViewPropertyAnimatorCompat changeNewAnimation(RecyclerView.ViewHolder holder) {
+        return ViewCompat.animate(holder.itemView).translationX(0).translationY(0).setDuration(getChangeDuration()).alpha(1).setInterpolator(getInterpolator());
     }
 
-    public void changeAnimationCleanup(View view) {
-        ViewCompat.setAlpha(view, 1);
+    public void changeAnimationCleanup(RecyclerView.ViewHolder holder) {
+        ViewCompat.setAlpha(holder.itemView, 1);
     }
 }
